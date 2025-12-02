864b6b74ebfb68d57ae2a3c6bd34f17c5de60d79
Xael <67170424+Xael@users.noreply.github.com>
2025-12-02 10:25:31 -0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,9 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    // Carrega variáveis de ambiente baseadas no modo (development/production)
     const env = loadEnv(mode, process.cwd(), '');
-    
     return {
       server: {
         port: 3000,
