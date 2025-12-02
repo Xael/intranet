d6446630950bc76c2e9bfe17d97e9614ad5a8d45
Xael <67170424+Xael@users.noreply.github.com>
2025-12-02 11:10:49 -0300

    Delete client/client directory

--- client/client/components/icons/PriceTagIcon.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const PriceTagIcon: React.FC<{className: string}> = (props) => (
-    <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M7 7h.01M7 3h5a2 2 0 012 2v5a2 2 0 01-2 2H7a2 2 0 01-2-2V5a2 2 0 012-2zM17 13h-5a2 2 0 00-2 2v5a2 2 0 002 2h5a2 2 0 002-2v-5a2 2 0 00-2-2z" />
-    </svg>
-);
